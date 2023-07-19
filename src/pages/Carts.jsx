6998e19ba6ee2f9264a70df565f04fb627641b9e
confidential-nt <youyoy689@gmail.com>
2023-07-19 18:39:24 +0900

    [feat] 1-2-2 구현

--- src/pages/Carts.jsx
+++ src/pages/Carts.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../context/UserContext";
 
 export default function Carts() {
+  const navigate = useNavigate();
+
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+
   return <div>여기는 계산페이지</div>;
 }
